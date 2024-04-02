b751c427c2e823d2c2084e1a9563904328d0c7c1
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2024-04-02 08:36:50 +0700

    update route method

--- routes/api.php
+++ routes/api.php
@@ -128,8 +128,8 @@ Route::middleware('jwt.verify')->group(function () {
             Route::get('/style', 'Api\Admin\ResourceAndDevelopment\StyleController@index');
             Route::post('/style/create', 'Api\Admin\ResourceAndDevelopment\StyleController@store');
             Route::get('/style/sub-style', 'Api\Admin\ResourceAndDevelopment\StyleController@getSubStyle');
-            Route::get('/style/sub-style/create', 'Api\Admin\ResourceAndDevelopment\StyleController@createSubStyle');
-            Route::get('/style/sub-style/{id}/delete', 'Api\Admin\ResourceAndDevelopment\StyleController@deleteSubStyle');
+            Route::post('/style/sub-style/create', 'Api\Admin\ResourceAndDevelopment\StyleController@createSubStyle');
+            Route::delete('/style/sub-style/{id}/delete', 'Api\Admin\ResourceAndDevelopment\StyleController@deleteSubStyle');
 
             // Fabric Texture's Route
             Route::post('/input-fabric-texture', 'Api\Admin\ResourceAndDevelopment\SampleProductController@inputFabricTexture');
